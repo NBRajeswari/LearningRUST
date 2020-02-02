1e6d14cd641b00d9d3fbe7b19f9dc165a552e826
Rajeswari <nbrajeswari@gmail.com>
2020-02-02 11:27:32 +0000

    Add draft functions

--- kwindex/src/main.rs
+++ kwindex/src/main.rs
@@ -1,3 +1,67 @@
+/// Each slice in this struct's list is a word in some
+/// in-memory text document.
+#[derive(Debug, Default, Clone)]
+pub struct KWIndex<'a>(Vec<&'a str>);
+
+impl<'a> KWIndex<'a> {
+
+    /// Make a new empty target words list.
+    pub fn new() -> Self {
+        KWIndex(Vec::new())
+    }
+    
+    /// Parse the `target` text and add the sequence of
+    /// valid words contained in it to this `KWIndex`
+    /// index.
+    ///
+    /// This is a "builder method": calls can be
+    /// conveniently chained to build up an index.
+    ///
+    /// Words are separated by whitespace or punctuation,
+    /// and consist of a span of one or more consecutive
+    /// letters (any UTF-8 character in the "letter" class)
+    /// with no internal punctuation.
+    ///
+    /// For example, the text
+    ///
+    /// ```text
+    /// "It ain't over untïl it ain't, over."
+    /// ```
+    ///
+    /// contains the sequence of words `"It"`, `"over"`,
+    /// `"untïl"`, `"it"`, `"over"`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let index = kwindex::KWIndex::new()
+    ///     .extend_from_text("Hello world.");
+    /// assert_eq!(2, index.len());
+    /// assert_eq!(1, index.count_matches("world"));
+    /// ```
+    pub fn extend_from_text(mut self, target: &'a str) -> Self {
+        self.0 = target.split(' ').collect();
+        self
+    }
+    
+    /// Count the number of occurrences of the given `keyword`
+    /// that are indexed by this `KWIndex`.
+    pub fn count_matches(&self, keyword: &str) -> usize {
+        4
+    }
+    
+    /// Count the number of words that are indexed by this
+    /// `KWIndex`.
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    
+    /// Is this index empty?
+    pub fn is_empty(&self) -> bool {
+        self.0.len() == 0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World")
 }
